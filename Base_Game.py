cbd04e2895a4bdaad9b2ca194d5c739f0270730c
Adam C <corwinadam@gmail.com>
2018-04-29 12:24:00 -0500

    Crated better game logic using a check dictionary. Added a running game state checking def.

--- Base_Game.py
+++ Base_Game.py
@@ -1,5 +1,6 @@
 import random
 
+
 # todo: improve game logic
 # todo: Clean up sentence syntax.
 
@@ -32,15 +33,20 @@ def game():
 def player_vs_ai_game_logic(player_name):
     p1_score = 0
     p2_score = 0
+    numofties = 0
+
     running = "yes"
     player_name = player_name
 
+    check_dict = {'rock:rock': 'Tied', 'paper:paper': 'Tied', 'scissors:scissors': 'Tied',
+                  'rock:scissors': 'Won', 'scissors:paper': 'Won', 'paper:rock': 'Won',
+                  'rock:paper': 'Lost', 'scissors:rock': 'Lost', 'paper:scissors': 'Lost'}
+
     # While the player wants to continue the game will run
     while True:
-
         # this selects the random AI choice
         ai_picks = ['rock', 'paper', 'scissors']
-        ai1_choice = random.choice(ai_picks)
+        choice_2 = random.choice(ai_picks)
 
         # if the player decides to stop the game will quit and print out the final score
         if running == "no":
@@ -49,188 +55,76 @@ def player_vs_ai_game_logic(player_name):
             break
 
         elif running == "yes":
-            player_choice = input("\nPlease pick rock, paper, or scissors ")
+            choice_1 = input("\nPlease pick rock, paper, or scissors ").lower()
+            combined_choice = choice_1 + ":" + choice_2
 
-            if player_choice.lower() == "rock":
-                running = "yes"
-                if ai1_choice == "paper":
-                    print("\nYou picked", player_choice, ",the AI picked", ai1_choice)
+            if check_dict.get(combined_choice) == "Won":
+                p1_score = p1_score + 1
+                print('Player 1 won this round, the current score is, Player 1: '
+                      , p1_score, 'Player 2:', p2_score, 'Ties:', numofties)
+                run_checker()
 
-                    p2_score = p2_score + 1
-                    print("\nYou lost this round,", player_name,
-                          "\n Your score is", p1_score, ",Player 2's Score is", p2_score)
+            elif check_dict.get(combined_choice) == "Lost":
+                p2_score = p2_score + 1
+                print('Player 2 won this round, the current score is, Player 1: '
+                      , p1_score, 'Player 2:', p2_score, 'Ties:', numofties)
+                run_checker()
 
-                    running = input("\nDo you want to play again?, Yes or No. ")
-                    running = running.lower()
-
-                elif ai1_choice == "scissors":
-                    print("\nYou picked", player_choice, ",the AI Picked", ai1_choice)
-                    p1_score = p1_score + 1
-                    print("\nYou won this round,", player_name,
-                          "\n Your score is", p1_score, ",Player 2's Score is", p2_score)
-
-                    running = input("\nDo you want to play again?, Yes or No. ")
-                    running = running.lower()
-                    print(running)
-
-                elif ai1_choice == "rock":
-                    print("\nYou picked", player_choice, ",the AI Picked", ai1_choice)
-                    print("\nThis round was a tie!,", player_name,
-                          "\n Player 1's score is", p1_score, ",Player 2's Score is", p2_score)
-
-                    running = input("\nDo you want to play again?, Yes or No. ")
-                    running = running.lower()
-
-            elif player_choice.lower() == "paper":
-                running = "yes"
-                if ai1_choice == "scissors":
-                    print("\nYou picked", player_choice, ",the AI picked", ai1_choice)
-
-                    p2_score = p2_score + 1
-                    print("\nYou lost this round,", player_name,
-                          "\n Your score is", p1_score, ",Player 2's Score is", p2_score)
-
-                    running = input("\nDo you want to play again?, Yes or No. ")
-                    running = running.lower()
-
-                elif ai1_choice == "rock":
-                    print("\nYou picked", player_choice, ",the AI Picked", ai1_choice)
-                    p1_score = p1_score + 1
-                    print("\nYou won this round,", player_name,
-                          "\n Your score is", p1_score, ",Player 2's Score is", p2_score)
-
-                    running = input("\nDo you want to play again?, Yes or No. ")
-                    running = running.lower()
-                    print(running)
-
-                elif ai1_choice == "paper":
-                    print("\nYou picked", player_choice, ",the AI Picked", ai1_choice)
-                    print("\nThis round was a tie!,", player_name,
-                          "\n Player 1's score is", p1_score, ",Player 2's Score is", p2_score)
-
-                    running = input("\nDo you want to play again?, Yes or No. ")
-                    running = running.lower()
-
-            elif player_choice.lower() == "scissors":
-                running = "yes"
-                if ai1_choice == "rock":
-                    print("\nYou picked", player_choice, ",the AI picked", ai1_choice)
-
-                    p2_score = p2_score + 1
-                    print("\nYou lost this round,", player_name,
-                          "\n Your score is", p1_score, ",Player 2's Score is", p2_score)
-
-                    running = input("\nDo you want to play again?, Yes or No. ")
-                    running = running.lower()
-
-                elif ai1_choice == "paper":
-                    print("\nYou picked", player_choice, ",the AI Picked", ai1_choice)
-                    p1_score = p1_score + 1
-                    print("\nYou won this round,", player_name,
-                          "\n Your score is", p1_score, ",Player 2's Score is", p2_score)
-
-                    running = input("\nDo you want to play again?, Yes or No. ")
-                    running = running.lower()
-                    print(running)
-
-                elif ai1_choice == "scissors":
-                    print("\nYou picked", player_choice, ",the AI Picked", ai1_choice)
-                    print("\nThis round was a tie!,", player_name,
-                          "\n Player 1's score is", p1_score, ",Player 2's Score is", p2_score)
-
-                    running = input("\nDo you want to play again?, Yes or No. ")
-                    running = running.lower()
+            elif check_dict.get(combined_choice) == "Tied":
+                numofties = numofties + 1
+                print('No one won this round, the current score is, Player 1: '
+                      , p1_score, 'Player 2:', p2_score, 'Ties:', numofties)
+                run_checker()
 
 
 # AI vs AI game logic portion
 def ai_vs_ai_game_logic():
     gamenum = 0
-    ai1_score = 0
-    ai2_score = 0
-    rounds_to_run = input("\n\nHow many games would you like to simulate? numbers only please")
+    p1_score = 0
+    p2_score = 0
+    numofties = 0
+    rounds_to_run = input("\n\nHow many games would you like to simulate? numbers only please\n")
     rounds_to_run = int(rounds_to_run)
 
+    check_dict = {'rock:rock': 'Tied', 'paper:paper': 'Tied', 'scissors:scissors': 'Tied',
+                  'rock:scissors': 'Won', 'scissors:paper': 'Won', 'paper:rock': 'Won',
+                  'rock:paper': 'Lost', 'scissors:rock': 'Lost', 'paper:scissors': 'Lost'}
 
     while True:
         if gamenum < rounds_to_run:
             ai_picks = ['rock', 'paper', 'scissors']
-            ai1_choice = random.choice(ai_picks)
-            ai2_choice = random.choice(ai_picks)
+            choice_1 = random.choice(ai_picks)
+            choice_2 = random.choice(ai_picks)
+            combined_choice = choice_1 + ":" + choice_2
 
-            if ai1_choice.lower() == "rock":
-                if ai2_choice == "paper":
-                    print("\nAI 1 picked", ai1_choice, ",AI 2 picked", ai2_choice)
+            if check_dict.get(combined_choice) == "Won":
+                p1_score = p1_score + 1
+                print('Player 1 won this round, the current score is, Player 1: ',
+                      p1_score, 'Player 2:', p2_score, 'Ties:', numofties)
+                gamenum = gamenum + 1
 
-                    ai2_score = ai2_score + 1
-                    print("\nAI 1 lost this round,",
-                        "\n AI 1's score is ", ai1_score, ",AI 2's score is", ai2_score)
+            elif check_dict.get(combined_choice) == "Lost":
+                p2_score = p2_score + 1
+                print('Player 2 won this round, the current score is, Player 1: ',
+                      p1_score, 'Player 2:', p2_score, 'Ties:', numofties)
+                gamenum = gamenum + 1
 
-                    gamenum = gamenum + 1
-
-                elif ai2_choice == "scissors":
-                    print("\nAI 1 picked", ai1_choice, ",AI 2 picked", ai2_choice)
-                    ai1_score = ai1_score + 1
-                    print("\nAI 1 won this round,",
-                         "\n AI 1's score is ", ai1_score, ",AI 2's score is", ai2_score)
-
-                    gamenum = gamenum + 1
-
-                elif ai2_choice == "rock":
-                    print("\nThis round was a tie. AI 1 picked", ai1_choice, ",AI 2 picked", ai2_choice)
-                    print("\nAI 1's score is ", ai1_score, ",AI 2's score is", ai2_score)
-
-                    gamenum = gamenum + 1
-
-            elif ai1_choice.lower() == "paper":
-                if ai2_choice == "rock":
-                    print("\nAI 1 picked", ai1_choice, ",AI 2 picked", ai2_choice)
-
-                    ai1_score = ai1_score + 1
-                    print("\nAI 2 lost this round,",
-                        "\n AI 1's score is ", ai1_score, ",AI 2's score is", ai2_score)
-
-                    gamenum = gamenum + 1
-
-                elif ai2_choice == "scissors":
-                    print("\nAI 1 picked", ai1_choice, ",AI 2 picked", ai2_choice)
-                    ai2_score = ai2_score + 1
-                    print("\nAI 2 won this round,",
-                        "\n AI 1's score is ", ai1_score, ",AI 2's score is", ai2_score)
-
-                    gamenum = gamenum + 1
-
-                elif ai2_choice == "paper":
-                    print("\nAI 1 picked", ai1_choice, ",AI 2 picked", ai2_choice)
-                    print("\nAI 1's score is ", ai1_score, ",AI 2's score is", ai2_score)
-
-                    gamenum = gamenum + 1
-
-            elif ai1_choice.lower() == "scissors":
-                if ai2_choice == "paper":
-                    print("\nAI 1 picked", ai1_choice, ",AI 2 picked", ai2_choice)
-
-                    ai1_score = ai1_score + 1
-                    print("\nAI 1 lost this round,",
-                        "\n AI 1's score is ", ai1_score, ",AI 2's score is", ai2_score)
-
-                    gamenum = gamenum + 1
-
-                elif ai2_choice == "rock":
-                    print("\nAI 1 picked", ai1_choice, ",AI 2 picked", ai2_choice)
-                    ai2_score = ai2_score + 1
-                    print("\nAI 2 won this round,",
-                            "\n AI 1's score is ", ai1_score, ",AI 2's score is", ai2_score)
-
-                    gamenum = gamenum + 1
-
-                elif ai2_choice == "scissors":
-                    print("\nAI 1 picked", ai1_choice, ",AI 2 picked", ai2_choice)
-                    print("\nAI 1's score is ", ai1_score, ",AI 2's score is", ai2_score)
-
-                    gamenum = gamenum + 1
+            if check_dict.get(combined_choice) == "Tied":
+                numofties = numofties + 1
+                print('No one won this round, the current score is, Player 1: ',
+                      p1_score, 'Player 2:', p2_score, 'Ties:', numofties)
+                gamenum = gamenum + 1
 
         elif gamenum == rounds_to_run:
             print("\n\nThe final score was:\n"
-                  "AI 1's score is", ai1_score, ", AI 2's score is", ai2_score)
+                  "AI 1's score is", p1_score, ", AI 2's score is", p2_score, "total ties are:", numofties)
             break
+
+
+def run_checker():
+    running = input("\nDo you want to play again?, Yes or No. ")
+    running = running.lower()
+    return running
+
+
 game()
